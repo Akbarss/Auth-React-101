4da1b83dec78857fd0bad2adafd72eeba9a3d1c5
Akbarss <kbrgulyamov@gmail.com>
2024-05-11 00:31:11 +0500

    remove useReducer

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,21 +1,17 @@
-import { useReducer, useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthData } from "../../auth/AuthWrapper";
 
 export const Login = () => {
   const navigate = useNavigate();
   const { login } = AuthData();
-  const [formData, setFormData] = useReducer(
-    (formData, newItem) => {
-      return { ...formData, ...newItem };
-    },
-    { userName: "", password: "" }
-  );
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState(null);
 
   const doLogin = async () => {
     try {
-      await login(formData.userName, formData.password);
+      await login(userName, password);
       navigate("/account");
     } catch (error) {
       setErrorMessage(error);
@@ -27,14 +23,10 @@ export const Login = () => {
       <h2>Login page</h2>
       <div className="inputs">
         <div className="input">
-          <input value={formData.userName} onChange={(e) => setFormData({ userName: e.target.value })} type="text" />
+          <input value={userName} onChange={(e) => setUserName(e.target.value)} type="text" />
         </div>
         <div className="input">
-          <input
-            value={formData.password}
-            onChange={(e) => setFormData({ password: e.target.value })}
-            type="password"
-          />
+          <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" />
         </div>
         <div className="button">
           <button onClick={doLogin}>Log in</button>
